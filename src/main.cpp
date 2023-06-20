8c52354685d611998a29199a1f16a94161f8949c
Juan Manuel <jibcande@gmail.com>
2023-06-20 23:15:06 +0200

    Final version to ship

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,5 @@ int main() {
   std::vector<std::string> cpu_data{};
 
   System system;
-  //TODO: Why the number of active processes is not properly displayed?
   NCursesDisplay::Display(system, 20);
 }
